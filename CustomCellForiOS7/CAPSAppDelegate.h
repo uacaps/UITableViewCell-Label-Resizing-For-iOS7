48e36eb5579ef33db7f69c22584b45a5f3febaf9
Ben Gordon <brgordon@ua.edu>
2013-10-09 14:25:54 -0500

    Initial Commit

---
+++ CustomCellForiOS7/CAPSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CAPSAppDelegate.h
+//  CustomCellForiOS7
+//
+//  Created by Ben Gordon on 10/9/13.
+//  Copyright (c) 2013 CAPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAPSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomCellForiOS7/CAPSViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAPSViewController.h
+//  CustomCellForiOS7
+//
+//  Created by Ben Gordon on 10/9/13.
+//  Copyright (c) 2013 CAPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAPSViewController : UIViewController
+
+@end
